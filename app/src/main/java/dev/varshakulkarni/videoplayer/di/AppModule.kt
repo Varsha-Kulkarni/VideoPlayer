7f17940771d04bb35e4f2d388f22e51b81e65ca9
Varsha Kulkarni <Varsha.Kulkarni@ril.com>
2023-03-02 22:46:24 +0530

    Delete History

--- app/src/main/java/dev/varshakulkarni/videoplayer/di/AppModule.kt
+++ app/src/main/java/dev/varshakulkarni/videoplayer/di/AppModule.kt
@@ -1,10 +1,14 @@
 package dev.varshakulkarni.videoplayer.di
 
 import android.app.Application
+import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import dev.varshakulkarni.videoplayer.utils.DataStoreRepository
+import dev.varshakulkarni.videoplayer.utils.DataStoreRepositoryImpl
 import javax.inject.Singleton
 
 @Module
@@ -13,4 +17,10 @@ class AppModule {
     @Singleton
     @Provides
     fun providesContentResolver(application: Application) = application.contentResolver
+
+    @Singleton
+    @Provides
+    fun provideDataStoreRepository(
+        @ApplicationContext app: Context
+    ): DataStoreRepository = DataStoreRepositoryImpl(app)
 }
